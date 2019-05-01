51d07f04e60bf713cef298ad9e0afa035847c70b
MOZAMMIL ATHAR <49124832+ThexRebel@users.noreply.github.com>
2019-05-01 10:10:34 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("\nenter two no. to be added : ");
+scanf("%d %d",&a,&b);
+printf("\nSum : %d",a+b);
+return 0;
+}
---
+++ w1-3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	printf("\nenter two no. : ");
+	scanf("%f %f",&a,&b);
+	printf("there product is :%f",(a*b));
+	return 0;
+}
